d4de3de940b569098be5214c5853f04615f1385b
wxtiago <tiagobatista.tec@gmail.com>
2021-11-07 05:37:59 -0300

    Parte 2

---
+++ app/src/containers/app/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+  state = {
+    input:''
+  }
+
+  render() {
+    return (
+      <h1>Hey</h1>
+    )
+  }
+}
+
+export default App;
